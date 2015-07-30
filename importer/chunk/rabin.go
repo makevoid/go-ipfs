dc4a48e93f85ec8aa3e504560bf6aee5cb355adc
Jeromy <jeromyj@gmail.com>
2015-07-30 15:18:31 -0700

    vendor correctly
    License: MIT
    Signed-off-by: Jeromy <jeromyj@gmail.com>

--- importer/chunk/rabin.go
+++ importer/chunk/rabin.go
@@ -4,7 +4,7 @@ import (
 	"hash/fnv"
 	"io"
 
-	"github.com/whyrusleeping/chunker"
+	"github.com/ipfs/go-ipfs/Godeps/_workspace/src/github.com/whyrusleeping/chunker"
 )
 
 var IpfsRabinPoly = chunker.Pol(17437180132763653)
